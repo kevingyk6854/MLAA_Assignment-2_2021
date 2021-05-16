1b2e7bcb1b73f466330c8073dd2660aa67afb174
kevin <kevingyk@163.com>
2021-05-16 10:43:32 +1000

    mod func

--- code/gbm_func.R
+++ code/gbm_func.R
@@ -10,6 +10,7 @@ library(caret)        # an aggregator package for performing many machine learni
 library(pdp)          # model visualization
 library(ggplot2)      # model visualization
 library(lime)         # model visualization
+library(corrplot)
 
 source(here("code", "data_trans.R"))
 
@@ -23,10 +24,11 @@ df <- df %>% relocate('default')
 set.seed(1234) 
 
 # Split training & test sets
-trainset_size <- floor(0.70 * nrow(df))
-trainset_indices <- sample(seq_len(nrow(df)), size = trainset_size)
-trainset <- df[trainset_indices, ]
-testset <- df[-trainset_indices, ]
+# trainset_size <- floor(0.70 * nrow(df))
+# trainset_indices <- sample(seq_len(nrow(df)), size = trainset_size)
+indices <- createDataPartition(df$default, p = .7, list = FALSE) 
+trainset <- df[indices, ]
+testset <- df[-indices, ]
 
 
 set.seed(1234)
@@ -35,7 +37,7 @@ formula_gbm <- as.formula(paste("default~", ".", sep = ""))
 
 # train GBM model
 gbm.fit <- gbm(
-  formula =  formula_gbm.,
+  formula =  formula_gbm,
   distribution = "bernoulli",
   data = trainset,
   n.trees = 10000,
@@ -87,7 +89,6 @@ hyper_grid <- expand.grid(
   interaction.depth = c(1, 3, 5),
   n.minobsinnode = c(5, 10, 15),
   bag.fraction = c(.65, .8, 1),
-  optimal_trees = 0,               # a place to dump results
   min_RMSE = 0                     # a place to dump results
 )
 
@@ -103,7 +104,7 @@ random_credit_train <- trainset[random_index, ]
 for(i in 1:nrow(hyper_grid)) {
   
   # reproducibility
-  set.seed(123)
+  set.seed(1234)
   
   # train model
   gbm.tune <- gbm(
@@ -116,13 +117,13 @@ for(i in 1:nrow(hyper_grid)) {
     n.minobsinnode = hyper_grid$n.minobsinnode[i],
     bag.fraction = hyper_grid$bag.fraction[i],
     train.fraction = .75,
+    # cv.folds = 5,
     n.cores = NULL, # will use all cores by default
     verbose = FALSE
   )
   
   # add min training error and trees to grid
-  hyper_grid$optimal_trees[i] <- which.min(gbm.tune$valid.error)
-  hyper_grid$min_RMSE[i] <- sqrt(min(gbm.tune$valid.error))
+  hyper_grid$min_RMSE[i] <- sqrt(min(gbm.tune$cv.error))
 }
 
 hyper_grid %>% 
@@ -130,6 +131,8 @@ hyper_grid %>%
   head(10)
 
 ##################
+
+set.seed(1234)
 
 # train GBM model
 gbm.fit.final <- gbm(
@@ -139,8 +142,8 @@ gbm.fit.final <- gbm(
   n.trees = 300,
   interaction.depth = 5,
   shrinkage = 0.1,
-  n.minobsinnode = 5,
-  bag.fraction = .65, 
+  n.minobsinnode = 10,
+  bag.fraction = 1.0, 
   train.fraction = 1,
   n.cores = NULL, # will use all cores by default
   verbose = FALSE
@@ -155,8 +158,35 @@ summary(
   las = 2
 )
 
+###########################
+
+features_by_gbm <- summary(
+  gbm.fit.final, 
+  cBars = 15,
+  method = relative.influence, # also can use permutation.test.gbm
+  las = 2
+)$var[1:9]
+formula_gbm2 <- as.formula(paste("default~", paste(features_by_gbm, collapse="+")))
+
+# train GBM model
+gbm.fit.final2 <- gbm(
+  formula = formula_gbm2,
+  distribution = "bernoulli",
+  data = trainset,
+  n.trees = 300,
+  interaction.depth = 5,
+  shrinkage = 0.1,
+  n.minobsinnode = 10,
+  bag.fraction = 1.0, 
+  train.fraction = 1,
+  n.cores = NULL, # will use all cores by default
+  verbose = FALSE
+)  
+
+###########################
+
 # predict values for test data
-pred <- predict(gbm.fit.final, n.trees = gbm.fit.final$n.trees, testset)
+pred <- predict(gbm.fit.final2, n.trees = gbm.fit.final2$n.trees, testset)
 
 library(ROCR)
 GBM_pred_testing <- prediction(pred, testset$default)
@@ -165,31 +195,47 @@ plot(GBM_ROC_testing)
 plot(GBM_ROC_testing, add=TRUE, col='blue')
 legend("right", legend = c("GBM"), col = c("blue"), lty = 1:2, cex = 0.6)
 
+prob <- predict(object = gbm.fit.final2,
+                n.trees = gbm.fit.final2$n.trees,
+                newdata = testset,
+                type = "response")
+
+gbm_analysis <- roc(response=testset$default, prob)
+plot(gbm_analysis, print.auc=TRUE, auc.polygon=TRUE, grid=c(0.1, 0.2),
+     grid.col=c("green", "red"), max.auc.polygon=TRUE,
+     auc.polygon.col="skyblue", print.thres=TRUE)
+
 # AUC
 auc.tmp <- performance(GBM_pred_testing, "auc")
 gbm_auc_testing <- as.numeric(auc.tmp@y.values)
-gbm_auc_testing # 0.8083886
+gbm_auc_testing # 0.8083886 0.8099489
 
 ###########################
 
+# partial dependence plots (PDPs)
+gbm.fit.final2 %>%
+  partial(pred.var = "SQRT_PAYX", n.trees = gbm.fit.final2$n.trees, grid.resolution = 100) %>%
+  autoplot(rug = TRUE, train = trainset)
 
-df_test <- transforming_data(read_csv(here::here("data/AT2_credit_test.csv")), 
-                             is_train = FALSE,
-                             cat_var_to_fact = FALSE) # Noted: gbm does not accept factor variables
+###########################
 
-pred <- predict(object = gbm.fit.final, 
-                n.trees = gbm.fit.final$n.trees, 
-                newdata = df_test, 
-                type = "response")
-
-df_t <- read_csv(here::here("data/AT2_credit_test.csv"))
-
-output_df <- data.frame(
-  ID = df_t %>% select(ID), 
-  default = pred
-)
-
-write_csv(output_df, 'data/gbm ver 1.1.1.csv')
+# df_test <- transforming_data(read_csv(here::here("data/AT2_credit_test.csv")), 
+#                              is_train = FALSE,
+#                              cat_var_to_fact = FALSE) # Noted: gbm does not accept factor variables
+# 
+# pred <- predict(object = gbm.fit.final, 
+#                 n.trees = gbm.fit.final$n.trees, 
+#                 newdata = df_test, 
+#                 type = "response")
+# 
+# df_t <- read_csv(here::here("data/AT2_credit_test.csv"))
+# 
+# output_df <- data.frame(
+#   ID = df_t %>% select(ID), 
+#   default = pred
+# )
+# 
+# write_csv(output_df, 'data/gbm ver 1.1.2.csv')
 
 
 
--- code/rf_test.R
+++ code/rf_test.R
@@ -22,22 +22,23 @@ credict_train_cleaned <- credict_train_cleaned %>% relocate('default')
 formula_rf <- as.formula(paste("default~", ".", sep = ""))
 
 # Random Forest Model Generation
-rf_model_generation <- function (df, formula_rf, target_col, only_pred=FALSE){
+rf_model_generation <- function (df, formula_rf, target_col, n_trees=100, only_pred=FALSE){
     
   # First step is to set a random seed to ensurre we get the same result each time
   set.seed(42) 
   
   # Split training & test sets
-  trainset_size <- floor(0.80 * nrow(df))
-  trainset_indices <- sample(seq_len(nrow(df)), size = trainset_size)
-  trainset <- df[trainset_indices, ]
-  testset <- df[-trainset_indices, ]
+  # trainset_size <- floor(0.80 * nrow(df))
+  # trainset_indices <- sample(seq_len(nrow(df)), size = trainset_size)
+  indices <- createDataPartition(df$default, p = .7, list = FALSE) 
+  trainset <- df[indices, ]
+  testset <- df[-indices, ]
   
   # Random Forest Model
   rf_model <- randomForest( formula_rf,
                              data = trainset, 
                              importance=TRUE, 
-                             ntree=100,
+                             ntree=n_trees,
                              norm.votes = TRUE) 
   
   default_pred <- predict(rf_model, testset) # prediction
@@ -66,22 +67,27 @@ rf_model_generation <- function (df, formula_rf, target_col, only_pred=FALSE){
     
     return (list(rf_model=rf_model, pred=default_pred, prob1=default_prob[, 2], 
                  confusion_table=conf_tab, accuracy=acc, 
-                 variable_importance=var_import, auc_value=auc_val))
+                 variable_importance=var_import, rf_analysis=rf_analysis, auc_value=auc_val))
   }
 
   
   return (list(rf_model=rf_model, prob1=default_prob[, 2]))
 }
 
-rf_mod <- rf_model_generation(credict_train_cleaned, formula_rf, target_col="default", only_pred=FALSE)
-rf_mod$auc_value # 0.7731
-
 ##############################################################################################################
 
+rf_mod <- rf_model_generation(credict_train_cleaned, formula_rf, target_col="default", n_trees=300, only_pred=FALSE)
+rf_mod$auc_value # 0.7903
+
+rf_mod2 <- rf_model_generation(credict_train_cleaned, formula_rf, target_col="default", n_trees=500, only_pred=FALSE)
+rf_mod2$auc_value # 0.7905
+
+##############################################################################################################
+# Use RFE to choose most important variables
 test_set = credict_train_cleaned
 
 # RFE
-control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+control <- rfeControl(functions=rfFuncs, method="cv", number=5)
 # run the RFE algorithm
 results <- rfe(test_set %>% select(-default), 
                test_set$default, 
@@ -96,18 +102,45 @@ plot(results, type=c("g", "o"))
 
 ##############################################################################################################
 
-# "AGE"       "PAY_1"     "EDUCATION" "LIMIT_BAL" "AGE_BIN"   "SQRT_PAYX" "BILL_AMT1" "BILL_AMT4"
+# "AGE"       "PAY_1"     "EDUCATION" "LIMIT_BAL" "AGE_BIN"   "SQRT_PAYX" "BILL_AMT1"
 features_by_rfe <- predictors(results)
 formula_rf2 <- as.formula(paste("default~", paste(features_by_rfe, collapse="+")))
   
-rf_mod2 <- rf_model_generation(credict_train_cleaned, formula_rf2, target_col="default", only_pred=FALSE)
-rf_mod2$auc_value # 0.7719
-rf_mod2$rf_model$confusion
+rf_mod3 <- rf_model_generation(credict_train_cleaned, formula_rf2, target_col="default", n_trees=300, only_pred=FALSE)
+rf_mod3$auc_value # 0.7811
+rf_mod3$rf_model$confusion
 #       0    1 class.error
-# 0 12524 1031  0.07606049
-# 1  2531 2355  0.51801064
+# 0 11004  866  0.07295703
+# 1  2232 2036  0.52296157
 
-rf_mod$rf_model$confusion
-#       0    1 class.error
-# 0 12476 1079  0.07960162
-# 1  2589 2297  0.52988129
+# plot AUC-ROC curve
+plot(rf_mod3$rf_analysis, print.auc=TRUE, auc.polygon=TRUE, grid=c(0.1, 0.2),
+     grid.col=c("green", "red"), max.auc.polygon=TRUE,
+     auc.polygon.col="skyblue", print.thres=TRUE)
+
+########################################################
+
+# credict_test_cleaned <- transforming_data(read_csv(here::here("data/AT2_credit_test.csv")),
+#                              is_train = FALSE,
+#                              cat_var_to_fact = TRUE) # Noted: gbm does not accept factor variables
+# 
+# levels(credict_test_cleaned$SQRT_PAYX) <- levels(credict_train_cleaned$SQRT_PAYX)
+# levels(credict_test_cleaned$PAY_3) <- levels(credict_train_cleaned$PAY_3)
+# levels(credict_test_cleaned$PAY_4) <- levels(credict_train_cleaned$PAY_4)
+# credict_test_cleaned$default <- 0
+# 
+# features_by_rfe <- predictors(results)
+# formula_rf <- as.formula(paste("default~", paste(features_by_rfe, collapse="+")))
+# 
+# probs <- predict(rf_mod3$rf_model, 
+#                  newdata = credict_train_cleaned,
+#                  type="prob")
+# 
+# df_t <- read_csv(here::here("data/AT2_credit_test.csv"))
+# 
+# output_df <- data.frame(
+#   ID = df_t %>% select(ID),
+#   default = probs
+# )
+# 
+# write_csv(output_df, 'data/rf ver 1.3.1.csv')
